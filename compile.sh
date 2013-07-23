8c60c44d2c54ea1de7e8c842ac9041058d6ae926
Long Ouyang <longouyang@gmail.com>
2013-07-22 23:17:07 -0700

    Generalize

--- compile.sh
+++ compile.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 
+# pandoc --toc \
+# --template template.html \
+# --read markdown \
+# --write html5 \
+# --mathjax \
+# --bibliography dev.bib \
+# --output generative-models \
+# generative-models.md
+
 pandoc --toc \
 --template template.html \
 --read markdown \
 --write html5 \
 --mathjax \
 --bibliography dev.bib \
---output generative-models \
-generative-models.md
+--output $1 \
+$1.md
+
 
 # TODO:
 # [ ] mathjax (vendor?)
 # [.] bibtex (still need csl style)
 # [.] template
 # [.] citations
+# [ ] editor injector
 
 
 
 
 ## potentially useful information
 ## modifying header identifiers: http://johnmacfarlane.net/pandoc/README.html#header-identifiers-in-html-latex-and-context
+
+## the docs are slightly inaccurate about how you can read out the title block meta data
+## you need to do
+## pandoc -s -t native test.txt
+## (the -s standalone flag is needed)
+## HT https://groups.google.com/d/msg/pandoc-discuss/poShPwMga9I/HOfn5fK1n9cJ
 
 ## http://johnmacfarlane.net/pandoc/README.html
 
@@ -30,3 +47,9 @@ generative-models.md
 
 # http://www.premiumpixels.com/freebies/apple-ios-linen-texture/
 # (grey wash wall) http://subtlepatterns.com/page/2/ 
+
+
+## IDEAS FOR HOW THE COMPILER WORKS
+# chapters.txt contains a list of markdown files
+# we read chapters.txt to build the chapter nav tool
+# template.html gets filled
