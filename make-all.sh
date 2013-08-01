44613a57a9d78d0c27ffddc14cd5244318b72535
Long Ouyang <longouyang@gmail.com>
2013-07-31 17:12:46 -0700

    Chapter numbering

--- make-all.sh
+++ make-all.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 python make-index.py
+i=1
 
-for file in $( ls *.md ); do
-    ./make-chapter.sh $file
+for file in $( cat chapters.txt ); do
+    ./make-chapter.sh $file $i
+    i=`expr $i + 1`
 done
+
+## TODO: compile files not specified
+## in chapters.txt
