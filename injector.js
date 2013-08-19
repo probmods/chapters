bf561940dec77c45bb91b3efd9b02fc2352a056b
Long Ouyang <longouyang@gmail.com>
2013-08-19 11:58:44 -0700

    Preliminary webchurch execution, communication with accounts server

--- injector.js
+++ injector.js
@@ -1,11 +1,22 @@
-/* global $, Cookies */
+/* global $, Cookies, console, require, CodeMirror */
 
-//
+var js_outmirror = null,
+    exec_outmirror = null,
+    church_to_js = require("./church_to_js").church_to_js,
+    church_builtins = require("./church_builtins"),
+    pr = require("./probabilistic/index"),
+    util = require("./probabilistic/util"),
+    transform = require("./probabilistic/transform");
+
+util.openModule(pr);
+util.openModule(church_builtins);
 
 // check if user is logged in
 (function() {
 
-  var loggedIn = Cookies.get("sessionid") || false;
+  // we can't use Cookies.get('sessionid') because that's an HTTPOnly
+  // cookie - it can't be read by client-side javascript
+  var loggedIn = Cookies.get("loggedin") || false;
 
   // return a dictionary of DOM element attributes
   var getAttributes = function(x) {
@@ -22,31 +33,101 @@
   };
 
   var injectEditor = function(item, text) {
-    var attributes = getAttributes(item); 
+    var attributes = getAttributes(item),
+        exerciseName = $(item).data("exercise");
+
+    // var $textArea = $('<textarea>')
+    //       .val(text)
+    //       .attr(attributes)
+    //       .attr("rows", text.split("\n").length + 1);
+    // $(item).replaceWith($textArea); 
+
+    // var editor = CodeMirror.fromTextArea($textArea[0], {
+    //   lineNumbers: true,
+    //   matchBrackets: true,
+    //   continueComments: "Enter",
+    //   viewportMargin: Infinity
+    // });
+
+    var editor = CodeMirror(
+      function(el) {$(item).replaceWith(el);},
+      {
+        value: text,
+        lineNumbers: true,
+        matchBrackets: true,
+        continueComments: "Enter",
+        viewportMargin: Infinity
+      });
+
+
+    var $container = $(editor.display.wrapper),
+        $results = $("<div class='results'>"),
+        $runButton = $("<button>").html("Run").click(function() {
+          var churchCode = editor.getValue();
+
+          if (!(typeof exerciseName == "undefined")) {
+
+            // asynchronously POST church code to /code/{exercise_name}
+            $.ajax({
+              type: "POST",
+              url: "/code/" + exerciseName + "/",
+              data: {
+                'new_code': churchCode
+              },
+              success: function() {
+                console.log("POST to /code/" + exerciseName + ": success");
+              },
+              error: function() {
+                console.log("POST to /code/" + exerciseName + ": failure");
+              }
+            });
+          }
+
+          try
+          {
+            var jsCode = church_to_js(churchCode);
+            jsCode = transform.probTransform(jsCode);
+
+            // asynchronously POST church results to /result/{exercise_name}
+            
+            
+            var resultText = JSON.stringify(eval(jsCode));
+            $results.removeClass("error").text(resultText);
+
+          }
+          catch (e)
+          {
+            var error = e.message;
+            $results.addClass("error").text( error );
+          }
+
+
+        });
+
+    $container.attr("id", "ex-"+ exerciseName).after( $results, $runButton  );
+
     
-    $(item).replaceWith(
-      $('<textarea>')
-        .val(text)
-        .attr(attributes)
-        .attr("rows", text.split("\n").length + 1)
-    ); 
+    
   };
 
 
   $(document).ready(function() {
     $("pre").map(function(index, item) {
-      if (!loggedIn) {
+      var exerciseName = $(item).attr("data-exercise");
+      
+      if (!loggedIn || !exerciseName) {
         injectEditor(item, $(item).text());
       } else {
-        var exerciseName = $(item).attr("data-exercise");
-        
+
         $.ajax({
           url: "/code/" + exerciseName + "/",
           success: function(text) {
+            console.log(text);
             injectEditor(item, text);
-          } 
-        }).fail(function() {
-          console.log("failure");
+          },
+          error: function() {
+            console.log("failure");
+          }
         });
 
         
