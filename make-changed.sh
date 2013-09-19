6321412e4f4d871dc41132d91830044d97a221d7
Noah Goodman <ngoodman@stanford.edu>
2013-09-19 09:51:34 -0700

    don't process uncomitted files

--- make-changed.sh
+++ make-changed.sh
@@ -8,14 +8,14 @@ i=1
 ## HT http://www.catonmat.net/blog/set-operations-in-unix-shell/
 ## this is utter sorcery.
 for stem in $( comm -23 <(ls *.md | sed -e 's/\.md//g' | sort) <( (echo index; cat chapters.txt) | sort ) ); do
-  if git status --porcelain | grep $stem
+  if git status --porcelain | grep $stem | grep 'M'
     then ./make-chapter.sh $stem
   fi
 done
 
 
 for file in $( cat chapters.txt ); do
-  if git status --porcelain | grep $file
+  if git status --porcelain | grep $file | grep 'M'
     then ./make-chapter.sh $file $i.; i=`expr $i + 1`
   fi
 done
