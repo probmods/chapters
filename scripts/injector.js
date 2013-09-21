8b54fe7ce02477cdb25cb5ee487851e5613720a4
Long Ouyang <longouyang@gmail.com>
2013-09-21 16:38:42 -0700

    Enable smart indent on Tab

--- scripts/injector.js
+++ scripts/injector.js
@@ -13,10 +13,10 @@ var format_result = require("./format_result").format_result;
 util.openModule(pr);
 util.openModule(church_builtins);
 
+CodeMirror.keyMap.default.Tab = "indentAuto";
+
 // check if user is logged in
 (function() {
-
-
   var runners = {};
   runners['webchurch'] = function(exerciseName, code, editor) {
     var $results = editor.$results;
