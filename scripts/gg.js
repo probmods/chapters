8065c7f7d03429d4b99d5fa80dee412f98b16308
Long Ouyang <longouyang@gmail.com>
2013-10-14 22:17:23 -0700

    Alert users when gg and session cookies are unsynced
    If gg is set but session is not, this can lead to code not being saved

---
+++ scripts/gg.js
@@ -0,0 +1,25 @@
+/* global Cookies */
+
+(function() {
+  $.ajax({
+    type: "GET",
+    url: "/account_status",
+    async: false,
+    success: function(x) {
+      if (!!x) {
+        if (!Cookies.get("gg")) {
+          Cookies.set("gg","1", {expires: 1234567});
+        }
+      } else {
+        if (Cookies.get("gg")) {
+          Cookies.expire("gg");
+          alert("Your login has expired. Re-login for code saving / loading");
+        }
+      }
+    },
+    error: function() {
+      alert('accounts server appears to be down. saving/loading code won\'t work');
+    }
+  });
+
+})();
