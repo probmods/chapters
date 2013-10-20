9d1403aea9483142a7b3f64586005d1f662ab9fb
Noah Goodman <ngoodman@stanford.edu>
2013-10-20 15:30:33 -0700

    fixed viz pipeline

--- scripts/viz.js
+++ scripts/viz.js
@@ -20,6 +20,37 @@ var listToArray = function(xs) {
   return a;
 };
 
+function format_result(obj) {
+	function format_result_in_list_helper(obj) {
+		if (obj.length == 2) {
+			return " " + format_result(obj[0]) + format_result_in_list_helper(obj[1]);
+		} else if (obj.length == 0) {
+			return ")";
+		} else {
+			return " . " + format_result(obj) + ")";
+		}
+	}
+    
+	if (Array.isArray(obj)) {
+		if (obj.length == 2) {
+			return "(" + format_result(obj[0]) + format_result_in_list_helper(obj[1]);
+		} else {
+			return "()";
+		}
+	} else {
+		if (typeof(obj) == "boolean") {
+			if (obj) {
+				return "#t";
+			} else {
+				return "#f";
+			}
+		} else {
+			return "" + obj;
+		}
+	}
+}
+
+
 var formatPercent = d3.format(".0%");
 
 function erinSort(array) {
