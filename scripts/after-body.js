9a7ab2b06d292e6f58a7692483c2faf06d586885
Long Ouyang <longouyang@gmail.com>
2014-07-15 13:39:21 -0700

    Forgot to add citation handling

--- scripts/after-body.js
+++ scripts/after-body.js
@@ -12,14 +12,52 @@
 
     // load google analytics
     if (!isLocal & "probmods.org".match(location.hostname)) {
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                                 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 
-    ga('create', 'UA-44328902-1', 'probmods.org');
-    ga('send', 'pageview');
+        ga('create', 'UA-44328902-1', 'probmods.org');
+        ga('send', 'pageview');
     }
 
+})()
+
+
+
+(function() {
+    $("#references ~ p > span:first-child")
+        .addClass("citekey")
+        .map(function(index, el) {
+            var key = $(el).text(),
+                $sourceNodes = $('span[data-cites="' + key + '"]'); // find spans in the text that reference this
+
+            $sourceNodes.map(function(j,y) {
+                var $y = $(y);
+                var targetNode = $(el.parentNode).clone().addClass("citation-expanded");
+                targetNode.find("span").remove(); 
+                
+                $y.append(targetNode);
+
+                // to handle touch events
+                if (false) {
+                    var unclickHandler, clickHandler;
+
+                    unclickHandler = function() {
+                        $y.removeClass('hover').one('click', clickHandler);
+                    };
+
+                    clickHandler = function() {
+                        $y.addClass('hover');
+                        setTimeout(function() {
+                            $(document).one('click', unclickHandler);
+                        }, 30);
+                    };
+
+                    
+                    $y.one('click', clickHandler);
+                }
+            }); 
+        });    
 })()
